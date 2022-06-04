a9d0fa24d89c36780cc2168833b6c12d5286f0d8
blrrubik <rubromvit@gmail.com>
2022-06-04 23:06:04 +0300

    add tests

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+
+insert into friend_status (name) values ('accepted');
+insert into mpa (name, description)  values ('a','a');
+insert into mpa (name, description)  values ('b','b');
+insert into mpa (name, description)  values ('c','c');
+insert into mpa (name, description)  values ('d','d');
+insert into mpa (name, description)  values ('e','e');
